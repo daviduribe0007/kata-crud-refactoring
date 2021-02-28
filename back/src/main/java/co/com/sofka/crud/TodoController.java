7626fe2ef6639e1d31d9990487e22e19428de36b
daviduribe0007 <daviduribe0007@gmail.com>
2021-02-28 09:22:14 -0500

    Aplicar reglas para no guardar elementos vácios

--- back/src/main/java/co/com/sofka/crud/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/TodoController.java
@@ -1,11 +1,12 @@
 package co.com.sofka.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
     @Autowired
@@ -18,15 +19,27 @@ public class TodoController {
     
     @PostMapping(value = "api/todo")
     public TodoDTO save(@Validated @RequestBody TodoDTO todoDTO){
-        return service.save(todoDTO);
+        if (validateBasicParameters(todoDTO).equals("")) {
+            try {
+                return service.save(todoDTO);
+            } catch (Exception exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+        throw new RuntimeException(validateBasicParameters(todoDTO));
     }
 
     @PutMapping(value = "api/todo")
     public TodoDTO update(@Validated @RequestBody TodoDTO todoDTO){
-        if(todoDTO.getId() != null){
-            return service.save(todoDTO);
+        if (validarParametersUpdate(todoDTO).equals("")) {
+            try {
+                service.save(todoDTO);
+            } catch (Exception exception) {
+                throw new RuntimeException(validarParametersUpdate(todoDTO)+exception);
+            }
         }
-        throw new RuntimeException("No existe el id para actualziar");
+        throw new RuntimeException(validarParametersUpdate(todoDTO));
+
     }
 
     @DeleteMapping(value = "api/{id}/todo")
@@ -36,7 +49,52 @@ public class TodoController {
 
     @GetMapping(value = "api/{id}/todo")
     public TodoDTO get(@PathVariable("id") Long id){
-        return service.get(id);
+        if (id > 0) {
+            try {
+                service.delete(id);
+            } catch (Exception exception) {
+                throw new RuntimeException("The character id only can contains positive numbers"+ exception);
+            }
+        }
+        throw new RuntimeException("");
     }
+
+    private String validarParametersUpdate(TodoDTO todo) {
+        String response = "";
+        response = validId(todo) ? "The id not exist to update" : validateBasicParameters(todo);
+        return response;
+    }
+
+    private String validateBasicParameters(TodoDTO todo) {
+        String response = "";
+
+        if (validateNull(todo.getName())) {
+            response = ("The name can't be null");
+        }
+        if (validateEmpty(todo.getName())) {
+            response = ("The name can't be empty");
+        }
+        if (validateNull(todo.getGroupListId())) {
+            response = ("The group list id can't be null");
+        }
+        if (validateEmpty(todo.getGroupListId())) {
+            response = ("The group list id can't be empty");
+        }
+        return response;
+    }
+
+    private boolean validateNull(String date) {
+        return date == null;
+    }
+
+    private boolean validateEmpty(String date) {
+        return date.isEmpty();
+    }
+
+    private Boolean validId(TodoDTO todo) {
+        return todo.getId() == null;
+    }
+
+
 
 }
